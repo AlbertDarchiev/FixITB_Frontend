04e670d6ee27a36e5f0c9ff315774366f9a998b4
Albert Darchiev <albert.darchiev.7e6@itb.cat>
2024-04-17 19:21:51 +0200

    GoogleJson - security

--- app/src/main/java/com/example/fixitb_frontend/ui/screens/IncidencesScreen.kt
+++ app/src/main/java/com/example/fixitb_frontend/ui/screens/IncidencesScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
