ba2c301ef173201f9cad8818e14006608751bd66
roman <muhammad-roman.aziz.7e5@itb.cat>
2024-04-18 16:39:56 +0200

    working on post incidencias, updated add topbar menu textfiel and checking pictures from camera

--- app/src/main/java/com/example/fixitb_frontend/ui/screens/PostIncidenceScreen.kt
+++ app/src/main/java/com/example/fixitb_frontend/ui/screens/PostIncidenceScreen.kt
@@ -1,5 +1,8 @@
 package com.example.fixitb_frontend.ui.screens
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -12,10 +15,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,15 +36,17 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.example.fixitb_frontend.R
+import com.example.fixitb_frontend.ui.theme.Blue1
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PostIncidenceScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Blue.copy(alpha = 0.5f))
-    ) {
+            .background(color = Blue1.copy(alpha = 1.0f))    ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -68,15 +82,169 @@ fun PostIncidenceScreen() {
                 )
             }
             Spacer(modifier = Modifier.height(20.dp))
+
+
+            //Comineza el dropdown menu para que dispositivo es
+
+            val listDispositiu = listOf("Torre","Monitor","Teclat","Ratoli")
+
+            var selectedText by remember { mutableStateOf(listDispositiu[0]) }
+
+            var isExpanded by remember {
+                mutableStateOf(false)
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+
+            ) {
+                ExposedDropdownMenuBox(
+                    expanded = isExpanded ,
+                    onExpandedChange = {isExpanded = !isExpanded}
+                ) {
+                    TextField(
+                        modifier = Modifier.menuAnchor(),
+                        value = selectedText ,
+                        onValueChange = {},
+                        label = { Text("Quin dispositiu es ?") } ,
+                        readOnly = true,
+                        trailingIcon = {ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)}
+                    )
+                    
+                    ExposedDropdownMenu(expanded = isExpanded, onDismissRequest = {isExpanded = false}) {
+                        listDispositiu.forEachIndexed{ index, text ->
+                            DropdownMenuItem(
+                                text = { Text(text = text) },
+                                onClick = {
+                                    selectedText = listDispositiu[index]
+                                    isExpanded = false
+                                },
+                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                            )
+                        }
+                    }
+                }
+                Text(
+                    text = "Currently selected: $selectedText",
+                    color = Color.White
+                )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+            //Acaba el dropdown menu para que dispositivo es
+
+//Comineza el dropdown menu para que dispositivo es
+
+            val listAula = listOf("Aula 101","Aula 201","Aula 301")
+
+            var selectedTextAula by remember { mutableStateOf(listAula[0]) }
+
+            var isExpandedAula by remember {
+                mutableStateOf(false)
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+
+            ) {
+                ExposedDropdownMenuBox(
+                    expanded = isExpandedAula ,
+                    onExpandedChange = {isExpandedAula = !isExpandedAula}
+                ) {
+                    TextField(
+                        modifier = Modifier.menuAnchor(),
+                        value = selectedTextAula ,
+                        onValueChange = {},
+                        label = { Text("Quina aula es ?") } ,
+                        readOnly = true,
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpandedAula)}
+                    )
+
+                    ExposedDropdownMenu(expanded = isExpandedAula, onDismissRequest = {isExpandedAula = false}) {
+                        listAula.forEachIndexed{ index, text ->
+                            DropdownMenuItem(
+                                text = { Text(text = text) },
+                                onClick = {
+                                    selectedTextAula = listAula[index]
+                                    isExpandedAula = false
+                                },
+                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                            )
+                        }
+                    }
+                }
+                Text(
+                    text = "Currently selected: $selectedTextAula",
+                    color = Color.White
+                )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+//Acaba el dropdown menu que aula es
+//Empiezan text fields
+            TextField(
+                value = "", // Ajusta el valor según sea necesario
+                onValueChange = { /* Notificar cambios */ },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text("Codi Dispositiu") }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+
+            TextField(
+                value = "", // Ajusta el valor según sea necesario
+                onValueChange = { /* Notificar cambios */ },
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text("Codi Movistar Dispositiu") }
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+
             TextField(
                 value = "", // Ajusta el valor según sea necesario
                 onValueChange = { /* Notificar cambios */ },
                 modifier = Modifier.fillMaxWidth(),
                 label = { Text("Ingrese su incidencia") }
             )
+            Spacer(modifier = Modifier.height(10.dp))
+
+//Acaba textfields
+// insertar imagen
+
+
+            val imageUri by remember { mutableStateOf<Uri?>(null) }
+
+            val takePicture = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess ->
+                if (isSuccess) {
+                    // La foto fue tomada exitosamente, puedes hacer algo con la Uri de la imagen
+                }
+            }
+            Button(
+                modifier = Modifier,
+                onClick = {
+                    // Lanzar la intención para tomar la foto
+                    takePicture.launch(imageUri)
+                }
+            ) {
+                Text("Tomar Foto")
+            }
+            imageUri?.let { uri ->
+                Image(
+                    painter = rememberAsyncImagePainter(uri),
+                    contentDescription = "Imagen tomada"
+                )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+
+
+
         }
     }
 }
+
+
 
 @Preview
 @Composable
